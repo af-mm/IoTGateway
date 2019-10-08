192373d7857d5b00b2eff5b3dab110972a83fefc
Mikhail Afanasev <af-mm@yandex.ru>
2019-10-08 16:15:37 +0300

    config.py added

---
+++ config.py
@@ -0,0 +1,10 @@
+
+EXTERNAL_MQTT_BROKER = {'host' : 'localhost',
+                        'port' : 1024}
+INTERNAL_MQTT_BROKER = {'host' : 'localhost',
+                        'port' : 1025}
+
+MAPPING = {
+    '0000000001/state' : 'kitchen/door/state',
+    '0000000002/state' : 'kitchen/kettle/state'
+}
