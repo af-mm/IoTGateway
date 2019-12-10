cc7beb25ca042ad81cd3a017c16d2a9c86fbe82a
BuildTools <unconfigured@null.spigotmc.org>
2019-12-10 18:14:14 +0300

    weatherStation.py added

---
+++ examples/weatherStation.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import time
+
+HOST = 'localhost'
+PORT = 20021
+TOPIC = '/room/weather_station/request'
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected with result code {}'.format(rc))
+
+client = mqtt.Client();
+client.on_connect = on_connect
+
+client.connect(HOST, PORT, 60)
+
+i = 0
+while True:
+    msg = 'lol_{}'.format(i)
+    i = i + 1
+    client.publish(TOPIC, msg)
+    client.loop(0.001)
+    print(msg)
+    time.sleep(30)
